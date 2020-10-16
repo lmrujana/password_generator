3ee71dc2daaab3e3267735c7efcc688e65d76105
Luis Rujana <lmrujanass@gmail.com>
2020-10-16 17:03:57 -0400

    Added for loop that randomly selects character

--- script.js
+++ script.js
@@ -23,13 +23,39 @@ function generatePassword (){
   var isSpecial = confirm('Do you want to add special characters?');
 
   if(passwordLength < 8 || passwordLength > 128){
-    alert('Invalid password length. Please start over.');
+    alert('Invalid password length. Please start over.'); //Length validation
   } else if(!isLowercase && !isUppercase && !isNumber && !isSpecial){
-    alert('No characters were selected. Please start over');
+    alert('No characters were selected. Please start over'); //Validates that at least one character type is selected
   } else {
-    
-    
-  }
+    //The if statements add the characters selected by the user to the activeChar array
+    if(isLowercase){
+      activeChar.push(lowerChar);
+    };
+
+    if(isUppercase){
+      activeChar.push(upperChar);
+    };
+
+    if(isNumber){
+      activeChar.push(numberChar);
+    };
+
+    if(isSpecial){
+      activeChar.push(specialChar);
+    };
+
+    var p_word = "";
+
+    //for loop that randomly selects a character and adds it to p_word
+    for(var i = 0; i <= passwordLength; i++){
+      var charTypeSelector = Math.floor(Math.random()*activeChar.length);
+      var charType = activeChar[charTypeSelector];
+      var charSelector = Math.floor(Math.random()*charType.length);
+      var char = charType[charSelector];
+      p_word = p_word + char;
+    };
+  };
+  return p_word;
 }
 
 // Write password to the #password input
