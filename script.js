d774e2143e42b6e2dfcc9ff6d6fcc382d7020cd8
Luis Rujana <lmrujanass@gmail.com>
2020-10-16 16:28:19 -0400

    Created prompts to input user preferences

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Variables that store the characters that we can use
 var specialChar = ' !"#$%&()*+,-./:;<=>?@[]^_`{|}~';
 var numberChar = '1234567890';
 var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
 var upperChar = 'ABCDEFGHIJKLMNOPQRESTUVWXYZ';
+
+//An array holding the character types selected by the user
 var activeChar = [];
+
+function generatePassword (){
+  //Variables that hold the user's preferences
+  var passwordLength = prompt('How many characters de you want? Enter a number between 8 and 128.');
+
+  var isLowercase = confirm('Do you want to add lowercase characters?');
+
+  var isUppercase = confirm('Do you want to add uppercase characters?');
+
+  var isNumber = confirm('Do you want to add numbers?');
+
+  var isSpecial = confirm('Do you want to add special characters?');
+
+  if(passwordLength < 8 || passwordLength > 128){
+    
+  }
 
 // Write password to the #password input
 function writePassword() {
