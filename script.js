75673d5c08f69fd03b6935098164b3eabedebbfa
Luis Rujana <lmrujanass@gmail.com>
2020-10-16 16:06:44 -0400

    Created variables to store characters

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialChar = ' !"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+var numberChar = '1234567890';
+var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+var upperChar = 'ABCDEFGHIJKLMNOPQRESTUVWXYZ';
+var activeChar = [];
 
 // Write password to the #password input
 function writePassword() {
